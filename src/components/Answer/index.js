e60c3d3f3956aca6c7d7ad6bb2b831938f732e54
Kelvin Stephano <kstephano@gmail.com>
2022-01-17 18:24:35 +0000

    added onClick

--- src/components/Answer/index.js
+++ src/components/Answer/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Answer = ({answer, index, correct}) => {
+const Answer = ({answer, index, correct, handleCorrect}) => {
     return (
         <div className="answer">
-            <input type="radio" name="answer" value={answer} id={"answer" + index} correct={correct ? "correct" : "incorrect"}/>
+            <input
+                type="radio"
+                name="answer"
+                value={answer} 
+                id={"answer" + (index + 1)} 
+                // correct={correct ? "correct" : "incorrect"}
+                onClick={() => handleCorrect(correct)}
+            />
             <label htmlFor={"answer" + index}>{answer}</label>
         </div>
     )
