5945883a59736b03cea7fc4dbc990fa1b4cc8d08
Kelvin Stephano <kstephano@gmail.com>
2022-01-17 11:22:39 +0000

    Added Routes and empty header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import { Home, Host, Leaderboard, Game } from "./pages"
+import { Header } from "./layout"
 
 const App = () => {
     return (
         <>
-            <h1>Hello world!</h1>
+            <Header />
+            <Routes>
+				<Route exact path="/" element={<Home />}></Route>
+				<Route path="/host" element={<Host />}></Route>
+                <Route path="/leaderboard" element={<Leaderboard />}></Route>
+                <Route path="/game/:lobbyId" element={<Game />}></Route>
+			</Routes>
         </>
     )
 }
