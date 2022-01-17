b29b63c26a6b8a855a27049cd2f7bb33fe7921c7
Kelvin Stephano <kstephano@gmail.com>
2022-01-17 15:31:26 +0000

    added provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
 
 import App from './App';
+import store from './redux/store';
 
 const rootElement = document.getElementById('root');
 
 render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>,
     rootElement
 );
