0e88bf504e1cb3c91599e482ce19913a9fb5d0e9
Kelvin Stephano <kstephano@gmail.com>
2022-01-17 11:26:53 +0000

    added data for dropdown lists

---
+++ src/data/index.js
@@ -0,0 +1,36 @@
+const categories = [
+    "Any Category",
+    "Animals",
+    "Art",
+    "Celebrities",
+    "Entertainment: Board Games",
+    "Entertainment: Books",
+    "Entertainment: Cartoons & Animation",
+    "Entertainment: Comics",
+    "Entertainment: Film",
+    "Entertainment: Japanese Anime & Manga",
+    "Entertainment: Music",
+    "Entertainment: Musicals & Theatre",
+    "Entertainment: Television",
+    "Entertainment: Video Games",
+    "General Knowledge",
+    "Geography",
+    "History",
+    "Mythology",
+    "Politics",
+    "Science & Nature ",
+    "Science: Computers",
+    "Science: Gadgets",
+    "Science: Mathematics",
+    "Sports"
+]
+
+const difficulty = [
+    "Easy",
+    "Medium",
+    "Hard"
+]
+
+const time = [ 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180 ]
+
+export { categories, difficulty, time }
