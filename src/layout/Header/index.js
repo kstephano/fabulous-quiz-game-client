8a41ac66b1524bfc95228a77d4de15ad6c67aae2
Kelvin Stephano <kstephano@gmail.com>
2022-01-17 15:32:30 +0000

    added Header

---
+++ src/layout/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Fabulous Quiz Game</h1>
+            <nav>
+                <NavLink to="/">Play</NavLink>
+                <NavLink to="/leaderboard">Leaderboard</NavLink>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
