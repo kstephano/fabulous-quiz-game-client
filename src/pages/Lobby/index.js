9eb959179c551f977892909ec5790d74c880a525
Kelvin Stephano <kstephano@gmail.com>
2022-01-18 09:24:56 +0000

    updated socket implementation in lobby

--- src/pages/Lobby/index.js
+++ src/pages/Lobby/index.js
@@ -1,24 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import io from 'socket.io-client';
 import { useSelector } from 'react-redux';
+
+const io = require('socket.io-client');
+const serverEndpoint = "http://localhost:4000";
+const socket = io(serverEndpoint);
 
 const Lobby = () => {
     const [ players, setPlayers ] = useState([]);
-    const [ socket, setSocket ] = useState(null);
     const name = useSelector(state => state.name);
     const params = useParams();
-    const serverEndpoint = "http://localhost:4000";
 
     useEffect(() => {
-        const newSocket = io(serverEndpoint);
-        console.log(newSocket);
-        setSocket(newSocket);
-        console.log(socket);
         socket.on("connection", () => {
             console.log("connected");
         });
-    
         /*
         socket.emit("joinLobby", name);
 
