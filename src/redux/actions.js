0b3828baeda88ba565d5bef89153c54f9af0538a
Kelvin Stephano <kstephano@gmail.com>
2022-01-18 13:42:43 +0000

    added lobby options to redux

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,24 @@
 // action types
-export const SET_NAME = "SET_NAME";
+const SET_NAME = "SET_NAME";
+const SET_HOST = "SET_HOST";
+const SET_LOBBY_OPTIONS = "SET_LOBBY_OPTIONS";
 
 // action creators
-export const setName = (name) => ({
+const setName = (name) => ({
     type: SET_NAME,
     payload: name
 });
+
+const setHost = (isHost) => ({
+    type: SET_HOST,
+    payload: isHost
+});
+
+const setLobbyOptions = (lobbyOptions) => ({
+    type: SET_LOBBY_OPTIONS,
+    payload: lobbyOptions
+});
+
+
+export { SET_NAME, SET_HOST, SET_LOBBY_OPTIONS }
+export { setName, setHost, setLobbyOptions }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,17 +1,29 @@
 import { combineReducers } from 'redux';
-import { SET_NAME } from "./actions";
+import { SET_NAME, SET_HOST, SET_LOBBY_OPTIONS } from "./actions";
 
-const reducer = (state="", action) => {
+const userReducer = (state={ name: "", isHost: false }, action) => {
     switch (action.type) {
         case SET_NAME:
-            return action.payload
+            return { ...state, name: action.payload }
+        case SET_HOST:
+            return { ...state, isHost: action.payload }
+        default:
+            return state;
+    }
+}
+
+const lobbyReducer = (state={ numOfQuestions: 5, category: "", difficulty: "", time: 60 }, action) => {
+    switch (action.type) {
+        case SET_LOBBY_OPTIONS:
+            return { state: action.payload }
         default:
             return state;
     }
 }
 
 const combinedReducer = combineReducers({
-    name: reducer
+    user: userReducer,
+    lobby: lobbyReducer
 });
 
 export default combinedReducer;
