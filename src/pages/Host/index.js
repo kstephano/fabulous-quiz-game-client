df2b2fccebf0aa19a80c24a8e72a615ce69b4ac9
Kelvin Stephano <kstephano@gmail.com>
2022-01-19 17:53:41 +0000

    changed category to categoryId

--- src/pages/Host/index.js
+++ src/pages/Host/index.js
@@ -6,7 +6,7 @@ import { setHost, setLobbyOptions } from '../../redux/actions';
 
 const Host = () => {
     const [ rounds, setRounds ] = useState(5);
-    const [ category, setCategory ] = useState(categories[0]);
+    const [ categoryId, setCategoryId ] = useState(8);
     const [ difficulty, setDifficulty ] = useState(difficulties[0]);
     const [ time, setTime ] = useState(times[0]);
     const navigate = useNavigate();
@@ -17,14 +17,17 @@ const Host = () => {
         dispatch(setHost(true));
         dispatch(setLobbyOptions({ 
             numOfQuestions: rounds,
-            category: category,
+            category: categoryId,
             difficulty: difficulty,
-            time: time 
+            roundLimit: time 
         }));
         navigate(`/lobby`);
     }
 
-    const renderOptions = (list) => list.map((item, key) => <option key={key} value={item}>{item}</option>);
+    // render options and set value to list item
+    const renderOptions = (list) => list.map((item, index) => <option key={index} value={item}>{item}</option>);
+    // render options and set value to list index
+    const renderCategories = (list) => list.map((item, index) => <option key={index} value={index + 8}>{item.name}</option>)
 
     return(
         <div className='host-container'>
@@ -33,7 +36,7 @@ const Host = () => {
                 <label htmlFor='rounds'>Number of questions</label>
                 <input onChange={e => setRounds(e.target.value)} type="number" id="rounds" name="rounds" step="1" min="5" max="30" />
                 <label htmlFor='category'>Category</label>
-                <select onChange={e => setCategory(e.target.value)} name="category" id="category">{renderOptions(categories)}</select>
+                <select onChange={e => setCategoryId(e.target.value)} name="category" id="category">{renderCategories(categories)}</select>
                 <label htmlFor='difficulty'>Difficulty</label>
                 <select onChange={e => setDifficulty(e.target.value)} name='difficulty' id='difficulty'>{renderOptions(difficulties)}</select>
                 <label htmlFor='time'>Time limit (seconds)</label>
